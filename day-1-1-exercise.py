b92fee20c27769b274910ba5a00e9c039daa7b31
Law <mychosh@gmail.com>
2022-03-23 16:58:44 +0900

    ADD day-1-1

---
+++ day-1-1-exercise.py
@@ -0,0 +1,7 @@
+############################################################
+# https://replit.com/@appbrewery/day-1-1-exercise#README.md
+############################################################
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
